21097154da4a474d476566bf41ba0a19b6832367
Roman-Shubnikov <62247180+Roman-Shubnikov@users.noreply.github.com>
2020-11-14 21:17:52 +0300

    fix

---
+++ mobile-road/build/precache-manifest.4d37c77cae33965ec25f7ba8a6af1033.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d9d5e0c739d9766aa74ea6d095d620",
+    "url": "/road/index.html"
+  },
+  {
+    "revision": "5d3f6c20b3dab68ce8f8",
+    "url": "/road/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "f1bf779770804e158f1e",
+    "url": "/road/static/css/main.588d2fc7.chunk.css"
+  },
+  {
+    "revision": "5d3f6c20b3dab68ce8f8",
+    "url": "/road/static/js/2.452c0c0b.chunk.js"
+  },
+  {
+    "revision": "cbe92b87e8caa8dc0e5f820d0532497f",
+    "url": "/road/static/js/2.452c0c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1bf779770804e158f1e",
+    "url": "/road/static/js/main.fcb2bb6f.chunk.js"
+  },
+  {
+    "revision": "50a76fc5c0e86ed6db9a",
+    "url": "/road/static/js/runtime-main.394b0f17.js"
+  },
+  {
+    "revision": "0c4d240df6e2e26c206537c704bced07",
+    "url": "/road/static/media/Ninja.0c4d240d.webp"
+  },
+  {
+    "revision": "57a77e9b55dbf653a85d1a65b0513efe",
+    "url": "/road/static/media/Soloriver.57a77e9b.mp3"
+  },
+  {
+    "revision": "7c18eb66ec99c11b7ce46c171a038364",
+    "url": "/road/static/media/jedi.7c18eb66.svg"
+  },
+  {
+    "revision": "14c5ae387cf9d1584ac5c292c64d797c",
+    "url": "/road/static/media/question_banner_mobile.14c5ae38.png"
+  },
+  {
+    "revision": "b2b8189baf7e3e0570e3b7cd87175523",
+    "url": "/road/static/media/question_banner_pc.b2b8189b.jpg"
+  },
+  {
+    "revision": "e2c1587721f383f373bf20c666eedb0b",
+    "url": "/road/static/media/schemeagent.e2c15877.jpg"
+  },
+  {
+    "revision": "6a49b893e3340373f387893dd919b580",
+    "url": "/road/static/media/start_slide_1.6a49b893.svg"
+  },
+  {
+    "revision": "fb768ec2870da4b8dce9709c6e58441a",
+    "url": "/road/static/media/start_slide_2.fb768ec2.svg"
+  },
+  {
+    "revision": "cfc7e0b10c7d95fc6cae56ed01200407",
+    "url": "/road/static/media/user.cfc7e0b1.jpg"
+  },
+  {
+    "revision": "76160308f2fa3bda0f3bedd7c338929a",
+    "url": "/road/static/media/verfload.76160308.svg"
+  }
+]);
